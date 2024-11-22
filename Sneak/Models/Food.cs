4fe2f4491c8c86d79621c646eb56dbb5317ce21e
njkita007@yandex.ru <e7MGrzDAdL>
2024-11-22 21:28:50 +0300

    Fix generate food

--- Sneak/Models/Food.cs
+++ Sneak/Models/Food.cs
@@ -4,20 +4,58 @@ using System.Drawing;
 
 namespace Sneak.Models
 {
+    /// <summary>
+    /// Класс, представляющий еду на игровом поле.
+    /// </summary>
     public class Food : GameObject
     {
-        private Random random;
+        private Random _random;
 
+        /// <summary>
+        /// Конструктор класса Food.
+        /// </summary>
+        /// <param name="width">Ширина игрового поля.</param>
+        /// <param name="height">Высота игрового поля.</param>
+        /// <param name="occupiedPositions">Список занятых позиций на поле.</param>
         public Food(int width, int height, List<Point> occupiedPositions)
-            : base(0, 0) // Временная инициализация позиции, будет изменена в GenerateNewPosition
+            : base(0, 0) // Инициализируем временную позицию, которая будет изменена
         {
-            random = new Random();
+            _random = new Random();
             GenerateNewPosition(width, height, occupiedPositions);
         }
 
+        /// <summary>
+        /// Генерирует новую позицию для еды, избегая занятых позиций.
+        /// </summary>
+        /// <param name="width">Ширина игрового поля.</param>
+        /// <param name="height">Высота игрового поля.</param>
+        /// <param name="occupiedPositions">Список занятых позиций.</param>
         public void GenerateNewPosition(int width, int height, List<Point> occupiedPositions)
         {
+            // Создаем список свободных клеток
+            List<Point> freeCells = GetFreeCells(width, height, occupiedPositions);
+
+            if (freeCells.Count == 0)
+            {
+                // Если нет свободных клеток, еда не может быть размещена
+                return;
+            }
+
+            // Выбираем случайную свободную позицию
+            Position = freeCells[_random.Next(freeCells.Count)];
+        }
+
+        /// <summary>
+        /// Получает список свободных клеток на игровом поле.
+        /// </summary>
+        /// <param name="width">Ширина игрового поля.</param>
+        /// <param name="height">Высота игрового поля.</param>
+        /// <param name="occupiedPositions">Список занятых позиций.</param>
+        /// <returns>Список свободных позиций.</returns>
+        private List<Point> GetFreeCells(int width, int height, List<Point> occupiedPositions)
+        {
             var freeCells = new List<Point>();
+
             for (int x = 0; x < width; x++)
             {
                 for (int y = 0; y < height; y++)
@@ -30,13 +68,7 @@ namespace Sneak.Models
                 }
             }
 
-            if (freeCells.Count == 0)
-            {
-                // Все клетки заняты, игра завершена
-                return;
-            }
-
-            Position = freeCells[random.Next(freeCells.Count)];
+            return freeCells;
         }
     }
 }
